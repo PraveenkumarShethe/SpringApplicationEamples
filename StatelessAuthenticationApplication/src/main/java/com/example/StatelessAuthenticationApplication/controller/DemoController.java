749e18f0adeffb1a7a63f9fbbc5ad2ea3bdbcb66
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-04-26 21:35:40 +0530

    Controller added

--- StatelessAuthenticationApplication/src/main/java/com/example/StatelessAuthenticationApplication/controller/DemoController.java
+++ StatelessAuthenticationApplication/src/main/java/com/example/StatelessAuthenticationApplication/controller/DemoController.java
@@ -1,13 +1,18 @@
 package com.example.StatelessAuthenticationApplication.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Praveenkumar on 4/26/2021.
  */
 @RestController
+@RequestMapping(value = "/demo", produces = MediaType.APPLICATION_JSON_VALUE)
 public class DemoController {
 
+    @RequestMapping(method = RequestMethod.GET, value = "/greetings", produces = MediaType.APPLICATION_JSON_VALUE)
     public String greetings(){
         return "Hello, Pal !!!";
     }

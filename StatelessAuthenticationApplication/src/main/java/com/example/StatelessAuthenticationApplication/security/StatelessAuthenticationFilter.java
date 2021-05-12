13c6ab319b0d72ee145b80bcecab417cf75d171c
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-05-12 10:55:44 +0530

    Authentication filter added

---
+++ StatelessAuthenticationApplication/src/main/java/com/example/StatelessAuthenticationApplication/security/StatelessAuthenticationFilter.java
@@ -0,0 +1,34 @@
+package com.example.StatelessAuthenticationApplication.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * Created by Praveenkumar on 5/11/2021.
+ */
+public class StatelessAuthenticationFilter extends GenericFilterBean {
+
+    private final TokenAuthenticationService tokenAuthenticationService;
+
+    public StatelessAuthenticationFilter(TokenAuthenticationService taService) {
+        this.tokenAuthenticationService = taService;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        SecurityContextHolder.getContext()
+                .setAuthentication(tokenAuthenticationService.getAuthenticationFromHeader((HttpServletRequest) request));
+        chain.doFilter(request, response);
+    }
+}

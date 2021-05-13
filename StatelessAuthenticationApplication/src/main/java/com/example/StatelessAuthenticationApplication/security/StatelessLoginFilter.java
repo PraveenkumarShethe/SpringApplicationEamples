d36ea0bfbec8dbaf03fa3d7096ab38c63941091f
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-05-13 11:54:50 +0530

    Login filter added

---
+++ StatelessAuthenticationApplication/src/main/java/com/example/StatelessAuthenticationApplication/security/StatelessLoginFilter.java
@@ -0,0 +1,84 @@
+package com.example.StatelessAuthenticationApplication.security;
+
+import com.example.StatelessAuthenticationApplication.model.MyUser;
+import com.example.StatelessAuthenticationApplication.repository.MyUserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Praveenkumar on 5/12/2021.
+ */
+public class StatelessLoginFilter extends AbstractAuthenticationProcessingFilter {
+    @Autowired
+    private TokenAuthenticationService tokenAuthenticationService;
+
+    @Autowired
+    private MyUserRepository upayogakartaRepository;
+
+    @Autowired
+    private MyUserDetailService myUserDetailService;
+
+    public StatelessLoginFilter(
+            String defaultFilterProcessesUrl,
+            TokenAuthenticationService tokenAuthenticationService,
+            MyUserDetailService myUserDetailService,
+            AuthenticationManager authenticationManager) {
+        super(new AntPathRequestMatcher(defaultFilterProcessesUrl));
+        this.myUserDetailService = myUserDetailService;
+        this.tokenAuthenticationService = tokenAuthenticationService;
+        setAuthenticationManager(authenticationManager);
+    }
+
+    public StatelessLoginFilter(String urlMapping,
+                                TokenAuthenticationService tokenAuthenticationService,
+                                AuthenticationManager authManager) {
+        super(new AntPathRequestMatcher(urlMapping));
+        this.tokenAuthenticationService = tokenAuthenticationService;
+        setAuthenticationManager(authManager);
+    }
+    protected StatelessLoginFilter(RequestMatcher requiresAuthenticationRequestMatcher) {
+        super(requiresAuthenticationRequestMatcher);
+    }
+    @Override
+    public Authentication attemptAuthentication(
+            HttpServletRequest request,
+            HttpServletResponse response)
+            throws AuthenticationException, IOException, ServletException {
+
+        final MyUser user = new ObjectMapper().readValue(request.getInputStream(), MyUser.class);
+        final UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                user.getUsername(), user.getPassword());
+
+        return getAuthenticationManager().authenticate(usernamePasswordAuthenticationToken);
+    }
+
+    //	@SuppressWarnings("serial")
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+                                            Authentication authentication) throws IOException, ServletException {
+        final MyUser user = myUserDetailService.getMyUser(myUserDetailService.loadUserByUsername(authentication.getName()));
+        final UserAuthentication userAuthentication = new UserAuthentication(user);
+        tokenAuthenticationService.addAuthenticationTokenInHeader(response, userAuthentication);
+        SecurityContextHolder.getContext().setAuthentication(userAuthentication);
+    }
+
+    protected StatelessLoginFilter(RequestMatcher requiresAuthenticationRequestMatcher, TokenAuthenticationService tokenAuthenticationService) {
+        super(requiresAuthenticationRequestMatcher);
+        this.tokenAuthenticationService = tokenAuthenticationService;
+    }
+}

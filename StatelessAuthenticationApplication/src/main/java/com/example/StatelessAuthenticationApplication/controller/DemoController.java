5af4d1715e0c749566353682aef5a819be7cc8fd
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-05-14 11:36:35 +0530

    demo controller updated with post method

--- StatelessAuthenticationApplication/src/main/java/com/example/StatelessAuthenticationApplication/controller/DemoController.java
+++ StatelessAuthenticationApplication/src/main/java/com/example/StatelessAuthenticationApplication/controller/DemoController.java
@@ -1,9 +1,14 @@
 package com.example.StatelessAuthenticationApplication.controller;
 
+import org.apache.tomcat.util.http.parser.MediaTypeCache;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.StatelessAuthenticationApplication.model.MyUser;
+import com.example.StatelessAuthenticationApplication.repository.MyUserRepository;
 
 /**
  * Created by Praveenkumar on 4/26/2021.
@@ -11,10 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/demo", produces = MediaType.APPLICATION_JSON_VALUE)
 public class DemoController {
+	
+	private MyUserRepository userRepo;
 
     @RequestMapping(method = RequestMethod.GET, value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
     public String greetings(){
         return "<h1>Hello, Pal !!!<h1>";
+    }
+    
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void saveMyUser(@RequestBody MyUser myUser) {
+    	userRepo.save(myUser);
     }
 
 }

bdb8bb3d1e5caf9eda51c8399b3fea4780aa47c4
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-05-16 09:09:28 +0530

    Spring boot kafka

---
+++ Spring_Boot_Apache_Kafka/src/main/java/com/praveenkumar/shethe/Spring_Boot_Apache_Kafka/SpringBootApacheKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.praveenkumar.shethe.Spring_Boot_Apache_Kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApacheKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApacheKafkaApplication.class, args);
+	}
+
+}
---
+++ Spring_Boot_Apache_Kafka/src/test/java/com/praveenkumar/shethe/Spring_Boot_Apache_Kafka/SpringBootApacheKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praveenkumar.shethe.Spring_Boot_Apache_Kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApacheKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

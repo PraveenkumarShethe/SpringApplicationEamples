89619dfd4096876a5cd45a01a5241ed6d28c176b
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-05-18 00:12:18 +0530

    KafkaService added with send

---
+++ Spring_Boot_Apache_Kafka/src/main/java/com/praveenkumar/shethe/Spring_Boot_Apache_Kafka/service/KafkaSender.java
@@ -0,0 +1,23 @@
+package com.praveenkumar.shethe.Spring_Boot_Apache_Kafka.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Praveenkumar on 5/17/2021.
+ */
+@Service
+public class KafkaSender {
+
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    String kafkaTopic = "message-topic";
+
+    public void send(String message) {
+
+        kafkaTemplate.send(kafkaTopic, message);
+    }
+}

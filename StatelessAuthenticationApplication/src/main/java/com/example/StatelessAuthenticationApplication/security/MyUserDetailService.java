d0788f52696f3c1b44333904abf0684250043a03
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-05-20 20:42:29 +0530

    logger added

--- StatelessAuthenticationApplication/src/main/java/com/example/StatelessAuthenticationApplication/security/MyUserDetailService.java
+++ StatelessAuthenticationApplication/src/main/java/com/example/StatelessAuthenticationApplication/security/MyUserDetailService.java
@@ -1,13 +1,15 @@
 package com.example.StatelessAuthenticationApplication.security;
 
-import com.example.StatelessAuthenticationApplication.repository.MyUserRepository;
 import com.example.StatelessAuthenticationApplication.model.MyUser;
+import com.example.StatelessAuthenticationApplication.repository.MyUserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.security.core.userdetails.UserDetails;
 
 /**
  * Created by Praveenkumar on 5/12/2021.
@@ -15,11 +17,14 @@ import org.springframework.security.core.userdetails.UserDetails;
 @Service
 public class MyUserDetailService implements UserDetailsService {
 
+    private static final Logger logger = LoggerFactory.getLogger(MyUserDetailService.class);
+
     @Autowired
     private MyUserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        logger.info(" loadUserByUsername ");
         User user;
         final MyUser myUser = userRepository.findByUsername(username);
         if (myUser != null) {
@@ -40,10 +45,12 @@ public class MyUserDetailService implements UserDetailsService {
     }
 
     public MyUser getMyUser(User user) {
+        logger.info(" getMyUser ");
         return userRepository.findByUsername(user.getUsername());
     }
 
     public MyUser getMyUser(UserDetails principal) {
+        logger.info(" getMyUser ");
         return getMyUser(
                 (User) loadUserByUsername(principal.getUsername()));
     }

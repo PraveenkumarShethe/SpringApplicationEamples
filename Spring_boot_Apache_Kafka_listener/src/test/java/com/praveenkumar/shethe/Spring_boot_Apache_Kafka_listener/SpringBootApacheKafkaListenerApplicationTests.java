d7422de9215f2c22929f4c76c784528ee9e411bb
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-05-22 11:34:19 +0530

    Application set up

---
+++ Spring_boot_Apache_Kafka_listener/src/test/java/com/praveenkumar/shethe/Spring_boot_Apache_Kafka_listener/SpringBootApacheKafkaListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praveenkumar.shethe.Spring_boot_Apache_Kafka_listener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApacheKafkaListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

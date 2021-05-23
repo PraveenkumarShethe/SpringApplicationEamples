c580e7de44e1ed68eb065dbfaa8b6ac499bbfa8f
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-05-23 20:06:32 +0530

    Project setup

---
+++ Spring_boot_Apache_Kafka_listener/src/main/java/com/praveenkumar/shethe/Spring_boot_Apache_Kafka_listener/SpringBootApacheKafkaListenerApplication.java
@@ -0,0 +1,13 @@
+package com.praveenkumar.shethe.Spring_boot_Apache_Kafka_listener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApacheKafkaListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApacheKafkaListenerApplication.class, args);
+	}
+
+}

1a75cf1d8bb52c07308a6cbfdc565841dd7a1fcb
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-05-24 21:04:52 +0530

    dependency added

--- Spring_boot_Apache_Kafka_listener/src/main/java/com/praveenkumar/shethe/Spring_boot_Apache_Kafka_listener/SpringBootApacheKafkaListenerApplication.java
+++ Spring_boot_Apache_Kafka_listener/src/main/java/com/praveenkumar/shethe/Spring_boot_Apache_Kafka_listener/SpringBootApacheKafkaListenerApplication.java
@@ -2,6 +2,7 @@ package com.praveenkumar.shethe.Spring_boot_Apache_Kafka_listener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient

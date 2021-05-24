32146c4f6fefb3e966115d19c533498b510b9240
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-05-25 00:15:06 +0530

    Enabled Eureka to application

--- EurekaDiscoveryServerClientExample/src/main/java/com/example/EurekaDiscoveryServerClientExample/EurekaDiscoveryServerClientExampleApplication.java
+++ EurekaDiscoveryServerClientExample/src/main/java/com/example/EurekaDiscoveryServerClientExample/EurekaDiscoveryServerClientExampleApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
 @EnableEurekaServer
+@SpringBootApplication
 public class EurekaDiscoveryServerClientExampleApplication {
 
 	public static void main(String[] args) {

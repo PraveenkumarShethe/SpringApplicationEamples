84d7b5f6d62883ac99affb38e33f997bb83ac877
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-05-25 00:14:22 +0530

    Controller added

---
+++ EurekaDiscoveryServerClientExample/src/main/java/com/example/EurekaDiscoveryServerClientExample/controller/ServiceRegistrationAndDiscoveryClientApplication.java
@@ -0,0 +1,27 @@
+package com.example.EurekaDiscoveryServerClientExample.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by Praveenkumar on 5/24/2021.
+ */
+@RestController
+public class ServiceRegistrationAndDiscoveryClientApplication {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
+
+}

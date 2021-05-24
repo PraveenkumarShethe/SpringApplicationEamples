b10307f2b8e1ce27497b4d6a0960cd5e7c1ccb5a
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-05-24 18:52:28 +0530

    Application properties abd EurakaDiscovery added

---
+++ EurekaDiscoveryServerClientExample/src/test/java/com/example/EurekaDiscoveryServerClientExample/EurekaDiscoveryServerClientExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EurekaDiscoveryServerClientExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryServerClientExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

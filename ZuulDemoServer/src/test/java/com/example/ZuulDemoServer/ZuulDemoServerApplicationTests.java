95705d2635b3d9e3f7cf796e995d196adee818ea
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-05-27 20:55:30 +0530

    Test file added

---
+++ ZuulDemoServer/src/test/java/com/example/ZuulDemoServer/ZuulDemoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ZuulDemoServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulDemoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

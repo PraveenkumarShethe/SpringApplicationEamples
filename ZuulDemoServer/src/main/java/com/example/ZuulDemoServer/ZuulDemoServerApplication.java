97668513b44c385cd62f2ca20dddd1b06ffe1f41
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-05-27 21:05:27 +0530

    Simple filter added for zuul configuration bean

--- ZuulDemoServer/src/main/java/com/example/ZuulDemoServer/ZuulDemoServerApplication.java
+++ ZuulDemoServer/src/main/java/com/example/ZuulDemoServer/ZuulDemoServerApplication.java
@@ -1,13 +1,22 @@
 package com.example.ZuulDemoServer;
 
+import com.example.ZuulDemoServer.filter.SimpleFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class ZuulDemoServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulDemoServerApplication.class, args);
+	}
+
+	@Bean
+	public SimpleFilter simpleFilter() {
+		return new SimpleFilter();
 	}
 
 }

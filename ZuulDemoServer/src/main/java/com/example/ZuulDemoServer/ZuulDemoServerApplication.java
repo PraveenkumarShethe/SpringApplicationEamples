aa85298c486f827f846d281bd0f2a970b7984b89
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-05-27 20:55:15 +0530

    Zuul server start up application file added

---
+++ ZuulDemoServer/src/main/java/com/example/ZuulDemoServer/ZuulDemoServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ZuulDemoServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulDemoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulDemoServerApplication.class, args);
+	}
+
+}

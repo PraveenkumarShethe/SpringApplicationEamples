48db4ac4c43bfbaf5e2fb847c37a97a059bdd4bc
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-05-28 21:56:04 +0530

    Client enabled with Zuul

--- ZuulDemoServer/src/main/java/com/example/ZuulDemoServer/ZuulDemoServerApplication.java
+++ ZuulDemoServer/src/main/java/com/example/ZuulDemoServer/ZuulDemoServerApplication.java
@@ -3,10 +3,12 @@ package com.example.ZuulDemoServer;
 import com.example.ZuulDemoServer.filter.SimpleFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ZuulDemoServerApplication {
 

e0af80eba35062c3c6eb4d0e88c16c81192c657b
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-05-28 21:55:18 +0530

    Zuul enable server added

--- ZuulDemo/src/main/java/com/example/ZuulDemo/ZuulDemoApplication.java
+++ ZuulDemo/src/main/java/com/example/ZuulDemo/ZuulDemoApplication.java
@@ -2,6 +2,7 @@ package com.example.ZuulDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
 public class ZuulDemoApplication {

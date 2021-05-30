c361efdb0f3928ff60ee644f2bb6b4e5c8502fd3
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-05-31 02:28:57 +0530

    Project set up added

---
+++ EmployeeGateWaySecurity/src/main/java/com/example/EmployeeGateWaySecurity/EmployeeGateWaySecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeGateWaySecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeGateWaySecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeGateWaySecurityApplication.class, args);
+	}
+
+}
---
+++ EmployeeGateWaySecurity/src/test/java/com/example/EmployeeGateWaySecurity/EmployeeGateWaySecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeGateWaySecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeGateWaySecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

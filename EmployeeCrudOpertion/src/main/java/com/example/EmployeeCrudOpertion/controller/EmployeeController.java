81b6507ccee5bdd74a9ad0c99ec8c49553886647
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-05-30 12:46:50 +0530

    Employee controller added, get all

---
+++ EmployeeCrudOpertion/src/main/java/com/example/EmployeeCrudOpertion/controller/EmployeeController.java
@@ -0,0 +1,30 @@
+package com.example.EmployeeCrudOpertion.controller;
+
+import com.example.EmployeeCrudOpertion.model.Employee;
+import com.example.EmployeeCrudOpertion.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by Praveenkumar on 5/30/2021.
+ */
+@RestController
+@RequestMapping(value = "employee")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public List<Employee> getAllEmployee(){
+        return employeeService.getAllEmployee();
+    }
+}

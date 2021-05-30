c64ed2c3a843e7a77547e6d896c9a388dab9535d
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-05-30 12:47:22 +0530

    Employee Service added

---
+++ EmployeeCrudOpertion/src/main/java/com/example/EmployeeCrudOpertion/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package com.example.EmployeeCrudOpertion.service;
+
+import com.example.EmployeeCrudOpertion.model.Employee;
+import com.example.EmployeeCrudOpertion.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by Praveenkumar on 5/30/2021.
+ */
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployee(){
+        return (List<Employee>) employeeRepository.findAll();
+    }
+
+}

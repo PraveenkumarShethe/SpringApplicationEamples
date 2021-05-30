9a0464ac7590f3e245010d52dc7c3eca4ebc7658
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-05-30 12:12:42 +0530

    Abstract Entity added

---
+++ EmployeeCrudOpertion/src/main/java/com/example/EmployeeCrudOpertion/model/AbstractEntity.java
@@ -0,0 +1,35 @@
+package com.example.EmployeeCrudOpertion.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.util.Objects;
+
+/**
+ * Created by Praveenkumar on 5/30/2021.
+ */
+@MappedSuperclass
+public abstract class AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AbstractEntity)) return false;
+        AbstractEntity that = (AbstractEntity) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}

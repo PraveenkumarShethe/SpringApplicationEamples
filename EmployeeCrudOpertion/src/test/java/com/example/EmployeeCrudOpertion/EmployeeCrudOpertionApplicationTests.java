363c104fa71a31734c4763ebef38be8e44d53179
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-05-30 11:46:21 +0530

    Test file

---
+++ EmployeeCrudOpertion/src/test/java/com/example/EmployeeCrudOpertion/EmployeeCrudOpertionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeCrudOpertion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCrudOpertionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

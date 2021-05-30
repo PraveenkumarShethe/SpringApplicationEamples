2bd1aa8c5e117fdd5e671dc1adda7d8e904fc91d
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-05-30 11:45:42 +0530

    Employee Spring start

---
+++ EmployeeCrudOpertion/src/main/java/com/example/EmployeeCrudOpertion/EmployeeCrudOpertionApplication.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeCrudOpertion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCrudOpertionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCrudOpertionApplication.class, args);
+	}
+
+}

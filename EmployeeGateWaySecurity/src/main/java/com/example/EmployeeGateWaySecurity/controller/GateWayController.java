1a51c712af6d2fa3408762acb0c633bcfca11d6e
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-06-04 22:28:10 +0530

    Removed gateway api

--- EmployeeGateWaySecurity/src/main/java/com/example/EmployeeGateWaySecurity/controller/GateWayController.java
+++ EmployeeGateWaySecurity/src/main/java/com/example/EmployeeGateWaySecurity/controller/GateWayController.java
@@ -1,18 +1,9 @@
 package com.example.EmployeeGateWaySecurity.controller;
 
-import com.example.EmployeeGateWaySecurity.model.Employee;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 /**
  * Created by Praveenkumar on 5/31/2021.
@@ -21,22 +12,6 @@ import reactor.core.publisher.Mono;
 @RequestMapping(value = "/auth")
 public class GateWayController {
 
-
     Logger logger = LoggerFactory.getLogger(GateWayController.class);
-    @Autowired
-    private WebClient webClient;
 
-    @RequestMapping(value = "/login", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseStatus(HttpStatus.OK)
-    public void employeeLogin(@RequestBody Employee employee) {
-        logger.info(" Employee login invoked");
-        logger.info(employee.getUsername() + "  ===============================  " + employee.getPassword());
-
-        webClient.post()
-                .uri("/employees")
-                .body(Mono.just(employee), Employee.class)
-                .retrieve()
-                .bodyToMono(Employee.class);
-        logger.info(employee.getUsername() + "  ===============================  " + employee.getPassword());
-    }
 }

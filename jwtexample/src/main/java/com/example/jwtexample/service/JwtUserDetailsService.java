49002c14f44a083b860765c9d70070cbd8165e1c
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-06-14 18:34:24 +0530

    Service layer

--- jwtexample/src/main/java/com/example/jwtexample/service/JwtUserDetailsService.java
+++ jwtexample/src/main/java/com/example/jwtexample/service/JwtUserDetailsService.java
@@ -15,8 +15,9 @@ import java.util.ArrayList;
 public class JwtUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println("LoadUserByUserName: " + username);
         if ("javainuse".equals(username)) {
-            return new User("javainuse", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+            return new User("javainuse", "$2a$10$zzp8hoFQHxj/d/yNM04m7uZ25CoX5ckfhYCaDI1za25HIDMDkZb9u",
                     new ArrayList<>());
         } else {
             throw new UsernameNotFoundException("User not found with username: " + username);

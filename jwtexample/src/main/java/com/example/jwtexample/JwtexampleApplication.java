3c83a5af1e9e565ad92aedfc63db8061a27b0555
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-06-14 18:34:08 +0530

    Web site

--- jwtexample/src/main/java/com/example/jwtexample/JwtexampleApplication.java
+++ jwtexample/src/main/java/com/example/jwtexample/JwtexampleApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JwtexampleApplication {
 
+
+	//https://www.javainuse.com/spring/boot-jwt example link
 	public static void main(String[] args) {
 		SpringApplication.run(JwtexampleApplication.class, args);
 	}
